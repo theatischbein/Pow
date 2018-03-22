3069e7cb4c82d10ffcd93a380c33257aa1ca7e23
Jonny Tischbein <jonny_tischbein@systemli.org>
2018-03-22 21:53:36 +0100

    Adding Fireball (Sprites, Animation, Particles)

---
+++ Fireball.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+const DIR = {
+	LEFT = 0, 
+	RIGHT = 1
+	}
+
+const SPEED = 400
+var offset_x = 50
+var velocity = Vector2(200,0)
+signal hit()
+
+func _ready():
+	pass
+		
+func shoot(var spawn, var dir):
+	if (dir == DIR.LEFT):
+		spawn.x -= offset_x
+		velocity = Vector2(-SPEED,0)
+		$AnimatedSprite.flip_h = true
+	elif (dir == DIR.RIGHT):
+		spawn.x += offset_x
+		velocity = Vector2(SPEED,0)
+		$AnimatedSprite.flip_h = false
+	
+	position = spawn
+	set_linear_velocity(velocity)
+	
+func destroy():
+	queue_free()
+	
+func contact(body):
+	set_linear_velocity(Vector2(0,0))
+	sleeping = true
+	$AnimatedSprite.hide()
+	$CollisionPolygon2D.hide()
+	$Particles2D.emitting = true
+	$Explosion.start()
+	
+func _on_Timer_timeout():
+	destroy()
+
+func _on_Explosion_timeout():
+	destroy()
--- player.gd
+++ player.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
+const DIR = {
+	LEFT = 0, 
+	RIGHT = 1
+	}
+
+export var last_dir = DIR.LEFT
 var force = Vector2()
 var direction_force = Vector2(0,0)
 var acc = 1000
@@ -9,27 +15,35 @@ var max_jump = 400
 func _ready():
 	pass
 
-func _integrate_forces(state):
-	
-	direction_force = Vector2(0,0)
+func _process(delta):
 	if (Input.is_action_pressed("ui_left")) :
-		direction_force += Vector2(-1,0)
-		$AnimatedSprite.flip_h = false;
-		
-	if (Input.is_action_pressed("ui_right")) :
-		direction_force += Vector2(1,0)
+		last_dir = DIR.LEFT
 		$AnimatedSprite.flip_h = true;
 		
-	if (Input.is_action_pressed("ui_up")) :
-		direction_force += Vector2(0,-1)
-
+	if (Input.is_action_pressed("ui_right")) :
+		last_dir = DIR.RIGHT
+		$AnimatedSprite.flip_h = false;
+		
 	if (direction_force.length() > 0):
 		$AnimatedSprite.play()
 		$Particles2D.emitting = true
 	else:
 		$AnimatedSprite.stop()
+		$AnimatedSprite.frame = 0
 		$Particles2D.emitting = false
+func _integrate_forces(state):
 	
+	direction_force = Vector2(0,0)
+	if (Input.is_action_pressed("ui_left")) :
+		direction_force += Vector2(-1,0)
+		
+	if (Input.is_action_pressed("ui_right")) :
+		direction_force += Vector2(1,0)
+		
+	if (Input.is_action_pressed("ui_up")) :
+		direction_force += Vector2(0,-1)
+
+
 	force = state.get_linear_velocity() + direction_force * acc
 	
 	if (force.x > max_move):
